25aeaf2a17e1f477afa9e40d8411225e4baa1074
Clarence HIRSCH <chirsch1@myges.fr>
2025-05-24 00:05:06 +0200

    feat: dynamic port setup

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,10 @@ async function bootstrap() {
   app.enableCors({
     origin: true,
   });
-  await app.listen(3000);
+  
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on port ${port}`);
 }
 
 bootstrap();
